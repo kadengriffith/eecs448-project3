989d66fc0be0f2c16758e93cd75b60b0097f49fb
Kaden Griffith <kadengriffith@ku.edu>
2017-10-25 14:53:15 -0500

    Time change back to 5 minutes. Addition of a 'game paused' flag for menus and time. Cleaned up some redundancy

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ let reloadOnTimeEnd = false; // After time length reload the window
 let loadTextures = false; // Only true when server-side
 let gravityConst = -9.81; /* -9.81 */
 // Time - Match length >= 1
-let minutes = 1; // See time.js
+let minutes = 5; // See time.js
 document.getElementsByClassName('time')[0].innerHTML = minutes + ":00";
 // @END INITIAL SETTINGS
 // CONTEXT & ENGINE
@@ -45,104 +45,93 @@ let scene = createScene();
 if(enableTime) { startSeconds(); } // See time.js
 // UPDATE LOOP
 engine.runRenderLoop(function () {
-  getScore(); // See time.js
-  // Easy in-game reset for debugging
-  // if(Player1.position.y < -20 || AI.position.y < -20) { window.location.reload(); }
-  // Reset the puck if goal
-  if(Puck.position.y < -20) {
-    Puck.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(0, 0, 0)); // Stop puck
-	  Puck.position.x = 0;
-    Puck.position.y = 10;
-    Puck.position.z = 0;
-    AI.position = new BABYLON.Vector3(0, player_yoff, (-ground_length / 2) + player_diameter);
-    Player1.position = new BABYLON.Vector3(0, player_yoff, (ground_length / 2) - player_diameter);
-  }
-  if(Puck.position.z > ground_length / 2 + puck_diameter) {
-    Puck.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(0, 0, 0)); // Stop puck
-	  Puck.position.x = 0;
-    Puck.position.y = 10;
-    Puck.position.z = 0;
-    AI.position = new BABYLON.Vector3(0, player_yoff, (-ground_length / 2) + player_diameter);
-    Player1.position = new BABYLON.Vector3(0, player_yoff, (ground_length / 2) - player_diameter);
-    setTimeout(score_blue++, 2000);
-  }
-  if(Puck.position.z < -(ground_length / 2) - puck_diameter) {
-    Puck.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(0, 0, 0)); // Stop puck
-	  Puck.position.x = 0;
-    Puck.position.y = 10;
-    Puck.position.z = 0;
-    AI.position = new BABYLON.Vector3(0, player_yoff, (-ground_length / 2) + player_diameter);
-    Player1.position = new BABYLON.Vector3(0, player_yoff, (ground_length / 2) - player_diameter);
-    setTimeout(score_red++, 2000);
-  }
-  // Check keys
-  if (key_D == true) {
-    Player1.position.x -= player_speed;
-  }
-  if (key_S == true) {
-    Player1.position.z += player_speed;
-  }
-  if (key_A == true) {
-    Player1.position.x += player_speed;
-  }
-  if (key_W == true) {
-    Player1.position.z -= player_speed;
-  }
-  if (key_P == true) {
-  	Puck.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(0, 0, 0)); // Stop puck
-    Puck.position.x = 0;
-    Puck.position.y = 10;
-    Puck.position.z = 0;
-  }
-  if (key_SPACE == true) {
-    player_speed = 0.66;
-  } else {
-    player_speed = 0.33;
-  }
-  // Display to the screen ~60fps
-
-  //prevent player1 from going to places that cause problems
-
-  if(PlayerOnBlueSide()) {
-    Player1.position.z += player_speed;
-  }
-
-  if(PlayerHittingBack()) {
-    Player1.position.z -= player_speed;
-  }
-
-  if(PlayerHittingLeft()) {
-    Player1.position.x -= player_speed;
-  }
-
-  if(PlayerHittingRight()) {
-    Player1.position.x += player_speed;
-  }
-
-  //control ai
-
-  AIFollowPuckX();
-
-  if(Puck.position.y > 11) {
-    AIReturn();
-  } else {
-
-    if(PuckOnBlueSide()) { //if the puck is on the blue side
-      if(Puck.position.z < AI.position.z) { //if the puck is behind the ai
-        if(Puck.position.x < 0) { //if the puck is on one side of the arena
-          AI.position.x -= ai_speed * 1.2;
+  if(!paused) { // Else -> present pause menu
+    getScore(); // See time.js
+    // Reset for debugging
+    // if(Player1.position.y < -20 || AI.position.y < -20) { window.location.reload(); }
+    // Reset the puck if goal
+    if(Puck.position.z > ground_length / 2 + puck_diameter) {
+      // Blue
+      Puck.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(0, 0, 0)); // Stop puck
+  	  dropPuck("CENTER");
+      AI.position = new BABYLON.Vector3(0, player_yoff, (-ground_length / 2) + player_diameter);
+      Player1.position = new BABYLON.Vector3(0, player_yoff, (ground_length / 2) - player_diameter);
+      setTimeout(score_blue++, 2000);
+    }
+    if(Puck.position.z < -(ground_length / 2) - puck_diameter) {
+      // Red
+      Puck.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(0, 0, 0)); // Stop puck
+      dropPuck("CENTER");
+      AI.position = new BABYLON.Vector3(0, player_yoff, (-ground_length / 2) + player_diameter);
+      Player1.position = new BABYLON.Vector3(0, player_yoff, (ground_length / 2) - player_diameter);
+      setTimeout(score_red++, 2000);
+    }
+    // Check keys
+    if (key_D == true) {
+      Player1.position.x -= player_speed;
+    }
+    if (key_S == true) {
+      Player1.position.z += player_speed;
+    }
+    if (key_A == true) {
+      Player1.position.x += player_speed;
+    }
+    if (key_W == true) {
+      Player1.position.z -= player_speed;
+    }
+    if (key_P == true) {
+    	Puck.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(0, 0, 0)); // Stop puck
+      dropPuck("CENTER");
+    }
+    if (key_SPACE == true) {
+      player_speed = 0.66;
+    } else {
+      player_speed = 0.33;
+    }
+    //prevent player1 from going to places that cause problems
+    if(PlayerOnBlueSide()) {
+      Player1.position.z += player_speed;
+    }
+    if(PlayerHittingBack()) {
+      Player1.position.z -= player_speed;
+    }
+    if(PlayerHittingLeft()) {
+      Player1.position.x -= player_speed;
+    }
+    if(PlayerHittingRight()) {
+      Player1.position.x += player_speed;
+    }
+    //control ai
+    AIFollowPuckX();
+    if(Puck.position.y > 11) {
+      AIReturn();
+    } else {
+      if(PuckOnBlueSide()) { //if the puck is on the blue side
+        if(Puck.position.z < AI.position.z) { //if the puck is behind the ai
+          if(Puck.position.x < 0) { //if the puck is on one side of the arena
+            AI.position.x -= ai_speed * 1.2;
+          } else {
+            AI.position.x += ai_speed * 1.2;
+          }
+          AI.position.z -= ai_speed * 1.2;
         } else {
-          AI.position.x += ai_speed * 1.2;
+          AI.position.z += ai_speed;
         }
-        AI.position.z -= ai_speed * 1.2;
-      } else {
-        AI.position.z += ai_speed;
       }
     }
+    // Display to the screen ~60fps
+    scene.render();
   }
-  scene.render();
 });
 // @END UPDATE LOOP
 //########################
 //  G A M E   E N D  :(
 //########################
+function dropPuck(loc) {
+  if(loc === "CENTER") {
+    Puck.position.x = 0;
+    Puck.position.y = 10;
+    Puck.position.z = 0;
+  }
+  return;
+}
--- src/time.js
+++ src/time.js
@@ -4,28 +4,31 @@
 let game_seconds = 0;
 let seconds = 0;
 // let minutes = 5; See main.js
+let paused = false;
 function startSeconds() {
   setInterval(function(){
-    if(seconds % 60 === 0) {
-      seconds = 0;
-      if(minutes > 0) { minutes--; } else {
-        return;
+    if(!paused) {
+      if(seconds % 60 === 0) {
+        seconds = 0;
+        if(minutes > 0) { minutes--; } else {
+          return;
+        }
       }
-    }
-    if(minutes < 0 && seconds < 1) {
-      document.getElementsByClassName("time")[0].innerHTML = "0:00";
-      if(reloadOnTimeEnd) { window.location.reload(); }
-    }else {
-      calculated_sec = 59 - seconds;
-      if(calculated_sec > 9) {
-        document.getElementsByClassName("time")[0].innerHTML = minutes + ":" + calculated_sec;
+      if(minutes < 0 && seconds < 1) {
+        document.getElementsByClassName("time")[0].innerHTML = "0:00";
+        if(reloadOnTimeEnd) { window.location.reload(); }
       }else {
-        document.getElementsByClassName("time")[0].innerHTML = minutes + ":0" + calculated_sec;
+        calculated_sec = 59 - seconds;
+        if(calculated_sec > 9) {
+          document.getElementsByClassName("time")[0].innerHTML = minutes + ":" + calculated_sec;
+        }else {
+          document.getElementsByClassName("time")[0].innerHTML = minutes + ":0" + calculated_sec;
+        }
       }
+      game_seconds++;
+      seconds++;
     }
-    game_seconds++;
-    seconds++;
-  }, 1000);
+  }, 1000 /* One Second */);
 }
 // Used for displaying score to HUD
 function getScore() {
