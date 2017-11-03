a22917b527e81de530a0ec06be237cfc60ae2084
Blaine Edmondson <blaine@ku.edu>
2017-11-02 19:43:39 -0500

    Added a connection to the server, plus a check to see if the teamColor variable and the pointAmounts variable are valid

--- highScoresPhpTemporary/highScoresRoughDraft.php
+++ highScoresPhpTemporary/highScoresRoughDraft.php
@@ -4,8 +4,35 @@
 //Update: table is live on blaines MYSQL server. "test" is the data base. (use test);
 <?php
 
+//might need to make this the 
 
-
+	$teamToUpdate = $_POST["teamColor"];
+	
+	$pointsToAdd = $_POST["pointsAdd"];
+	
+	//eventually this will be kadens server
+	$data_base = new mysqli("mysql.eecs.ku.edu", "bedmondson", 'P@$$word.123', "test");
+	
+	if($data_base->connect_errno)
+	{
+		printf("Unable to reach server: %s\n", $data_base->connect_error);
+		//add return to post game or main game page?
+		exit();
+	}
+	
+	if(strlen($teamToUpdate) == 0)
+	{
+		echo "Something went wrong, Try again later.";
+		//add return to post game or main game page?
+		exit();
+	}
+	
+	if( $pointsToAdd == 0)
+	{
+		echo "No points to add? Something went wrong. (line #null) ";
+		//add return to post game or main game page?
+		exit();
+	}
 
 
 ?>
