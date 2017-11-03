96155d4b359ea583e6aec1a9ed1c1ff7be0b230c
Blaine Edmondson <blaine@ku.edu>
2017-11-02 19:30:59 -0500

    Updated table status

--- highScoresPhpTemporary/highScoresRoughDraft.php
+++ highScoresPhpTemporary/highScoresRoughDraft.php
@@ -1,6 +1,7 @@
 //getting the template ready
 //Line used to create the table below... (on my EECS MYSQL server)
 //CREATE TABLE Teams ( team_id int NOT NULL AUTO_INCREMENT, team_color text NOT NULL, team_score int NOT NULL, team_wins int NOT NULL, team_losses int NOT NULL, PRIMARY KEY (team_id) );
+//Update: table is live on blaines MYSQL server. "test" is the data base. (use test);
 <?php
 
 
