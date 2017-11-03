b7dcc7fb9f05d2b0e45758e49890380f2ce0a714
Blaine Edmondson <blaine@ku.edu>
2017-11-02 19:24:22 -0500

    added a comment for the MYSQL table creation for when we move to Kaden's Server. Will use the same line for testing.

--- highScoresPhpTemporary/highScoresRoughDraft.php
+++ highScoresPhpTemporary/highScoresRoughDraft.php
@@ -1,4 +1,6 @@
 //getting the template ready
+//Line used to create the table below... (on my EECS MYSQL server)
+//CREATE TABLE Users ( team_id int NOT NULL AUTO_INCREMENT, team_color text NOT NULL, team_score int NOT NULL, team_wins int NOT NULL, team_losses int NOT NULL, PRIMARY KEY (team_id) );
 <?php
 
 
